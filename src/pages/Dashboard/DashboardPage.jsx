acf190b2bce8a14403146baf667a3f2ee8b04eed
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-07-02 11:02:09 +0600

    created dashboard overview page

---
+++ src/pages/Dashboard/DashboardPage.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+const DashboardPage = () => {
+   return (
+      <div className="max-w-4xl mx-auto mt-10 p-6 bg-white shadow-lg rounded-lg">
+         <h1 className="text-4xl font-semibold text-gray-800 mb-2">
+            Welcome to Dashboard Admin!
+         </h1>
+         <p className="text-gray-600 mb-4">
+            Manage your application smoothly and efficiently.
+         </p>
+         <div className="flex justify-between items-center">
+            <Link
+               to="/dashboard/overview"
+               className="btn text-[#FF2E00] hover:bg-purple-700 hover:text-white shadow-md shadow-indigo-500/40 ease-in-out duration-500 px-8 py-4 mb-5"
+            >
+               Get Started
+            </Link>
+         </div>
+      </div>
+   );
+};
+
+export default DashboardPage;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,7 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import DashboardLayout from "../layouts/DashboardLayout";
 import MainLayout from "../layouts/MainLayout";
-import BlogPage from "../pages/BlogPage";
-import HomePage from "../pages/HomePage";
+import BlogPage from "../pages/Main/BlogPage";
+import DashboardOverview from "../pages/Dashboard/DashboardOverview";
+import DashboardPage from "../pages/Dashboard/DashboardPage";
+import HomePage from "../pages/Main/HomePage";
 import LoginPage from "../pages/LoginPage";
 
 export const router = createBrowserRouter([
@@ -16,6 +19,20 @@ export const router = createBrowserRouter([
          {
             path: "/blogs",
             element: <BlogPage />,
+         },
+      ],
+   },
+   {
+      path: "/dashboard",
+      element: <DashboardLayout />,
+      children: [
+         {
+            path: "/dashboard",
+            element: <DashboardPage />,
+         },
+         {
+            path: "/dashboard/overview",
+            element: <DashboardOverview />,
          },
       ],
    },
