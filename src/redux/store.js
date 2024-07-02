dc40f8be55126bbdb8a7d89aa51db73cace2aaab
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-07-02 18:13:13 +0600

    added authSlice to redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../redux/features/auth/authSlice.js";
 
 export const store = configureStore({
-   reducer: {},
+   reducer: {
+      auth: authReducer,
+   },
 });
