404dc60fa17b82091a100867a57b81ad1dee4f03
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-07-02 13:12:48 +0600

    implemented responsiveness to dashboard overview page

--- src/components/Dashboard/QuickActions.jsx
+++ src/components/Dashboard/QuickActions.jsx
@@ -3,7 +3,7 @@ import ActionButton from "./ActionButton";
 const QuickActions = () => (
    <div className="mb-8">
       <h3 className="text-xl font-semibold mb-3">Quick Actions</h3>
-      <div className="flex gap-4">
+      <div className="flex flex-col md:flex-row gap-4">
          <ActionButton color="blue" text="Add Blog Post" />
          <ActionButton color="green" text="Add Experience" />
          <ActionButton color="purple" text="Add Project" />
