00bc3302c4edb8c41c0abf36b4ff6a75b936176b
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-08-19 16:42:51 +0600

    update: social icons

--- src/components/Main/ConnectWithMe.jsx
+++ src/components/Main/ConnectWithMe.jsx
@@ -1,12 +1,12 @@
-import { AiFillGithub } from "react-icons/ai";
-import { FaFacebookF, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaFacebookF, FaGithub } from "react-icons/fa";
+import { FaLinkedinIn, FaXTwitter } from "react-icons/fa6";
 
 const ConnectWithMe = () => {
    return (
       <div>
          <h3 className="mb-3">Connect With Me</h3>
          <a
-            className="btn text-[#0866ff] hover:bg-purple-700 hover:text-white shadow-md shadow-indigo-500/40 ease-in-out duration-500 px-5 py-3 rounded mr-3"
+            className="btn text-[#0866FF] hover:bg-purple-700 hover:text-white shadow-md shadow-indigo-500/40 ease-in-out duration-500 px-5 py-3 rounded mr-3"
             href="https://www.facebook.com/mehedihasan.mubin"
             target="_blank"
             rel="noreferrer"
@@ -19,7 +19,7 @@ const ConnectWithMe = () => {
             target="_blank"
             rel="noreferrer"
          >
-            <AiFillGithub className="text-md" />
+            <FaGithub className="text-md" />
          </a>
          <a
             className="btn text-[#0a66c2] hover:bg-purple-700 hover:text-white shadow-md shadow-indigo-500/40 ease-in-out duration-500 px-5 py-3 rounded mr-3"
@@ -27,15 +27,15 @@ const ConnectWithMe = () => {
             target="_blank"
             rel="noreferrer"
          >
-            <FaLinkedin className="text-md" />
+            <FaLinkedinIn className="text-md" />
          </a>
          <a
-            className="btn text-[#1da1f2] hover:bg-purple-700 hover:text-white shadow-md shadow-indigo-500/40 ease-in-out duration-500 px-5 py-3 rounded"
+            className="btn text-black hover:bg-purple-700 hover:text-white shadow-md shadow-indigo-500/40 ease-in-out duration-500 px-5 py-3 rounded"
             href="https://twitter.com/hasanmubin98"
             target="_blank"
             rel="noreferrer"
          >
-            <FaTwitter className="text-md" />
+            <FaXTwitter className="text-md" />
          </a>
       </div>
    );
